80ed3b50a2e284f236ac26e8e1d22fbbf0957314
mac <mac@MacBook-Pro-cua-mac-2.local>
2024-06-11 14:15:34 +0700

    Added func

---
+++ C2.py
@@ -0,0 +1,11 @@
+list_numbers = [12, 8, 13, 9, 11]
+
+def find_largestnumber:
+    largest_number = 0
+    for i in list_numbers:
+        if i > largest_number:
+        largest_number = i
+        return
+
+
+    
